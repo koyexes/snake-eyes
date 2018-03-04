be5800a918ff5525c6ff54b18ca0984e3a22bc36
andela-akoya <adegboyega.koya@andela.com>
2018-03-04 17:28:59 +0100

    implement page blueprint

---
+++ snakeeyes/page/views.py
@@ -0,0 +1,17 @@
+from flask import render_template
+from . import page
+
+
+@page.route('/')
+def home():
+    return render_template('page/home.html')
+
+
+@page.route('/terms')
+def terms():
+    return render_template('page/terms.html')
+
+
+@page.route('/privacy')
+def privacy():
+    return render_template('page/privacy.html')
